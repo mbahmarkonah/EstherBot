0c08f46964fc53d89fa44a0d0d7cb1356995033f
mbahmarkonah <mbah.markonah@gmail.com>
2016-08-22 11:35:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,8 @@ module.exports = new Script({
     },
 
     start: {
-        prompt: (bot) => {
-            bot.say('Halo saya DoBot \u{1F916}')
-            .then(() => 'speak');
+        prompt: (bot) => bot.say('Halo saya DoBot \u{1F916}')
+        receive: () => 'speak');
         }
     },
     
