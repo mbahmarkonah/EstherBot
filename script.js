b269ee3b44e5f245725c2a36269631fb92c9acf6
mbahmarkonah <mbah.markonah@gmail.com>
2016-09-07 15:38:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -49,6 +49,16 @@ module.exports = new Script({
                 
                 if (!_.has(scriptRules, upperText)) {
                     
+                    if (upperText == String('KONVERSI')){
+                        return bot.say('Silahkan masukkan data DP dalam mm');
+                        receive: (bot, message) => {
+                            var konversi = message.text;
+                            konversi = konversi * 0,0393701;
+                            return bot.say('DP adalah' + konversi + ' inch')
+                            .then(() => 'speak');
+                        }
+                    }
+                    
                     if (upperText == String('/START')){
                     return bot.say('Start Dobot \u{1F916}').then(() => 'start');
                     }
